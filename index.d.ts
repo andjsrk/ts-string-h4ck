b571d35e200786892704cce60ca227d8112ac8af
andjsrk <andjsrk0213@gmail.com>
2025-03-14 13:04:30 +0900

    initial commit

---
+++ index.d.ts
@@ -0,0 +1,46 @@
+/**
+ * Preserves suggestions while accepts any strings.
+ * 
+ * See also: https://github.com/microsoft/TypeScript/issues/29729
+ * 
+ * @example
+ * ```ts
+ * type FontWeight = 'lighter' | 'normal' | 'bold' | StringHack
+ * 
+ * //                          v caret here
+ * const weight: FontWeight = '|'
+ * //                          'lighter'
+ * //                          'normal'
+ * //                          'bold'
+ * const dynamicWeight: FontWeight = 'var(--my-weight)' // ok
+ * ```
+ */
+export type StringHack = string & NonNullable<unknown>
+/**
+ * Excludes {@linkcode StringHack} from the union.
+ * 
+ * @example
+ * ```ts
+ * type FontWeight = 'lighter' | 'normal' | 'bold' | StringHack
+ * 
+ * type LiteralFontWeight = ExcludeStringHack<FontWeight>
+ * // LiteralFontWeight = 'lighter' | 'normal' | 'bold'
+ * ```
+ */
+export type ExcludeStringHack<U> =
+	U extends string
+		? string extends U
+			? never
+			: U
+		: U
+type ExtractStringHack<U> =
+	Exclude<U, ExcludeStringHack<U>>
+/**
+ * Intersects two unions without getting simplified to `string`.
+ */
+export type IntersectWithoutLoss<A, B> =
+	// NOTE: we extract `StringHack` instead of just doing `| StringHack`
+	// because although user used this type, someday the both types may not have `StringHack`
+	(ExcludeStringHack<A> & ExcludeStringHack<B> | ExtractStringHack<A | B>) extends infer T
+		? T
+		: never
---
+++ test.d.ts
@@ -0,0 +1,62 @@
+import { ExcludeStringHack, IntersectWithoutLoss, StringHack } from './index'
+
+type Expect<_T extends ToBe, ToBe> = void
+
+declare namespace Hack {
+	// check if `string` gets simplified
+	namespace Str {
+		type T = 'a' | string
+		type Checks = [
+			Expect<string, T>,
+		]
+	}
+	// check if `StringHack` gets simplified
+	namespace Hack {
+		type Equal<A, B> = (<G>() => G extends A ? 0 : 1) extends (<G>() => G extends B ? 0 : 1) ? true : false
+		
+		type T = 'a' | StringHack
+		type Checks = [
+			Expect<string, T>,
+			Expect<Equal<string, T>, false>,
+			Expect<Equal<string & {}, T>, false>,
+		]
+	}
+}
+declare namespace ExcludeHack {
+	namespace Basic {
+		type T = ExcludeStringHack<'a' | 'b' | StringHack>
+		type Checks = [
+			Expect<T, 'a' | 'b'>,
+			Expect<'a' | 'b', T>,
+		]
+	}
+	namespace TemplateString {
+		type T = ExcludeStringHack<'a' | `b${string}` | StringHack>
+		type Checks = [
+			Expect<T, 'a' | `b${string}`>,
+			Expect<'a' | `b${string}`, T>,
+		]
+	}
+	namespace WithOtherPrimitive {
+		type T = ExcludeStringHack<0 | 'a' | StringHack>
+		type Checks = [
+			Expect<T, 0 | 'a'>,
+			Expect<0 | 'a', T>,
+		]
+	}
+}
+declare namespace Intersect {
+	namespace Basic {
+		type T = IntersectWithoutLoss<'a' | 'b' | StringHack, 'b' | 'c' | StringHack>
+		type Checks = [
+			Expect<ExcludeStringHack<T>, 'b'>,
+			Expect<StringHack, T>,
+		]
+	}
+	namespace WithoutHack {
+		type T = IntersectWithoutLoss<'a' | 'b', 'b' | 'c'>
+		type Checks = [
+			Expect<T, 'b'>,
+		]
+	}
+}
